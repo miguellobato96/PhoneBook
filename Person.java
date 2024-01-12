d1e2b5e0555c8c1c2ace09eaafc50102172ca39f
Miguel Lobato <149892700+miguellobato96@users.noreply.github.com>
2024-01-12 03:16:46 +0000

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,51 @@
+public class Person {
+    private String name;
+    private String phoneNumber;
+    private String email;
+    private String address;
+
+    // Construtor
+    public Person(String name, String phoneNumber, String email, String address) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
+    }
+
+    // Getters e Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String toString() {
+        return "Nome: " + name + ", Número de Telefone: " + phoneNumber + ", Email: " + email + ", Morada: " + address;
+    }
+}
